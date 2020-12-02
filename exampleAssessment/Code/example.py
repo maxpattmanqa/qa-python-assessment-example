87b8cefe6b54e131a7d6297b345ed56203e21443
maxpattmanqa <mpattman@qa.com>
2020-12-02 15:36:33 +0000

    pytest still not working , trying to understand why

--- exampleAssessment/Code/example.py
+++ exampleAssessment/Code/example.py
@@ -14,5 +14,9 @@
 	# What was the name of the function we have seen which changes the case of a string?  Use your CLI to access the Python documentation and get help(str).
     
 def endsPy(input):
-	# code here
+	#take slice of input for the two last letters to = py
+	if (input[-2:] == 'py'):
+		return True
+	else:
+		return False
 	
